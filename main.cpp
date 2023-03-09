de4eec7dc586651767d2fb9b4742c133682bbb27
David S <124046476+David-Soremekun@users.noreply.github.com>
2023-03-09 11:17:20 +0000

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <thread>
-#include "Loop.h"
-int main()
-{
-    std::thread([]{std::cout << "Hello from thread!\n";}).join();
-    std::cout << "Hello World!\n";
-    return 0;
-}
